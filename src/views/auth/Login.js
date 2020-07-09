f1679a6357ff9d7fa6751efca7bcbe275fdb91c9
Ruben A. Batista Santos <alexbatista158@hotmail.com>
2020-07-08 23:08:26 -0400

    .

---
+++ src/views/auth/Login.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import { withRouter } from 'react-router-dom';
+
+
+
+const Login = ()=> {
+
+    return (
+        <Fragment>
+            <h2>Este es el login</h2>
+        </Fragment>
+    )
+}
+
+export default withRouter(Login);
---
+++ src/views/projects/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+
+const Projects= ()=> {
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+return withRouter(Projects)
